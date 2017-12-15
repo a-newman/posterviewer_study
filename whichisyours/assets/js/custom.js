4b72b7059b5586205c6a9cd9e4aeab4e3adae881
Anelise Newman <apnewman@mit.edu>
2017-12-15 02:21:20 -0500

    added whichisyours with playing gif on hover

--- whichisyours/assets/js/custom.js
+++ whichisyours/assets/js/custom.js
@@ -1,3 +1,5 @@
+var POSTER_USED;
+
 function gup(name) {
     var regexS = "[\\?&]"+name+"=([^&#]*)";
     var regex = new RegExp( regexS );
@@ -7,15 +9,54 @@ function gup(name) {
     else return results[1];
 }
 
+function parse_matching_file(str) {
+    str = str.trim();
+    var pairings = str.split("\n");
+    var mapping = {}
+    pairings.forEach(function(elt, i) {
+        var arr = elt.split(", ");
+        mapping[arr[0]] = arr[1];
+    })
+    return mapping;
+}
+
+function playGif() {
+    console.log("playing gif");
+    $(this).find('.gif').show();
+    $(this).find('.static-img').hide();
+}
+
+function hideGif() {
+    console.log("hiding gif");
+    $(this).find('.gif').hide();
+    $(this).find('.static-img').show();
+}
+
 $(document).ready(function() {
+    $('.gif').hide();
     var name = gup("name");
     var id = gup("id");
     $(".name").text(name);
     $(".id").text(id);
 
-    var base = "http://people.csail.mit.edu/kimberli/poster/data/" + name + "/WhichYours/" + id;
-    $("#img1").attr("src", base + "/A.gif");
-    $("#img2").attr("src", base + "/B.gif");
-    
+    // figure out which poster this cooresponds to 
+    $.ajax({
+        url: "data/" + name + "/WhichYours/files_used.txt", 
+        dataType: "text"
+    }).then(function(data) {
+        var mapping = parse_matching_file(data); 
+        var poster = mapping[id].trim() + '.jpg';
+        var posterUrl = "assets/imgs/" + poster;
+        // fill in the static images with the original poster, resized 
+        $('.static-img').attr('src', posterUrl);
+    });
+
+
+    // fill in the images 
+    var base = "data/" + name + "/WhichYours/" + id;
+    $("#gif1").attr("src", base + "/A.gif");
+    $("#gif2").attr("src", base + "/B.gif");
+
+    $('.gif-container').hover(playGif, hideGif);    
 
 });
